a2e377195ef0d0c8acd94c47765a701a9273da11
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-08-06 19:35:12 -0600

    finshed home-routes and created connected handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-        const PostData = await Post.findAll({
+        const postData = await Post.findAll({
             attributes: ['id', 'title', 'text', 'created_at'],
             include: [
                 {
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
             ],
         });
 
-        const posts = PostData.map((post) => post.get({ plain: true }));
+        const posts = postData.map((post) => post.get({ plain: true }));
         res.render('homepage', {
             posts,
             loggedIn: req.session.loggedIn
@@ -45,6 +45,74 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: { id: req.params.id },
+            attributes: ['id', 'text', 'title', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'body', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['name'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
+        if (!postData) {
+            res.status(404).json({ message: 'Can not find post!' });
+            return;
+        }
+
+        const post = postData.get({ plain: true });
+        console.log(post);
+        res.render('getpostbyid', { post, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+router.get('/postswithcomments', async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: { id: req.params.id },
+            attributes: ['id', 'text', 'title', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'body', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['name'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'Can not find post!' });
+            return;
+        }
+
+        const post = postData.get({ plain: true });
+        res.render('postswithcomments', { post, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
